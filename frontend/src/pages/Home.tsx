dee1bfe289d71c942688286d7e9b7c4a5e667099
Kevin <kevinglennon01@gmail.com>
2024-03-05 18:50:58 +0000

    Just working on adding stuff to the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-
+import Styles from "../styles/home.module.scss";
+import Efficency from "../components/statistics/Efficency";
+import Overview from "../components/statistics/Overview";
 function Home() {
   return (
-    <div>
-      <div>You have logged in!</div>
-    </div>
+    <>
+      <div className={Styles.container}>
+        <div className={Styles.wrapper}>
+          <p className={Styles.title}>Welcome Username</p>
+          <div className={Styles.boxContainer}>
+            <div className={Styles.box}>
+              <Overview />
+            </div>
+            <div className={Styles.box}>
+              <Efficency />
+            </div>
+            <div className={Styles.box}>Box 3</div>
+            <div className={Styles.box}>Box 4</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
