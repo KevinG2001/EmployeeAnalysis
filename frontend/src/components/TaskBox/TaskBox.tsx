2887df7e7c6848d9ed108f4d0f128155de4e1584
Kevin <kevinglennon01@gmail.com>
2024-03-22 14:05:08 +0000

    Panels now show depending on what div is clicked (basically a button)

--- frontend/src/components/TaskBox/TaskBox.tsx
+++ frontend/src/components/TaskBox/TaskBox.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Tasks from "./Tasks";
 import Styles from "../../styles/Tasks.module.scss";
 
 function TaskBox() {
+  const [panelPointer, setPanelPointer] = useState("Completed");
+  const handleClick = (e) => {
+    const divName = e.target.textContent;
+    setPanelPointer(divName);
+  };
+
   return (
     <>
       <div className={Styles.container}>
         <div className={Styles.sidebar}>
-          <div>Completed Tasks</div>
-          <div>Available Tasks</div>
+          <div onClick={handleClick} className={Styles.taskBtn}>
+            Completed Tasks
+          </div>
+          <div onClick={handleClick} className={Styles.taskBtn}>
+            Available Tasks
+          </div>
+          <div onClick={handleClick} className={Styles.taskBtn}>
+            Your Tasks
+          </div>
         </div>
         <div className={Styles.contentPanel}>
-          {/* When One of the buttons is clicked it will show whats needed */}
-          {/* Example.. List of tasks assigned to you or completed tasks */}
-          <Tasks />
+          {panelPointer === "Completed Tasks" ? (
+            <Tasks />
+          ) : panelPointer === "Available Tasks" ? (
+            <div>Available Tasks</div>
+          ) : (
+            panelPointer === "Your Tasks" && <div>Your Tasks</div>
+          )}
         </div>
       </div>
     </>
