53fb98c2119d0cad202a14bc350654d59b47e0c5
Kevin <kevinglennon01@gmail.com>
2024-04-14 21:39:46 +0100

    Logout modal now added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,15 +4,46 @@ import Statistics from "../components/statistics/StatisticsBox";
 import Overview from "../components/statistics/Overview";
 import TaskBox from "../components/TaskBox/TaskBox";
 import { useUser } from "../util/userUtil";
+import profilePic from "../assets/profilePicPlaceholder.svg";
+import LogoutModal from "../components/modals/LogoutModal";
 
 function Home() {
   const user = useUser();
+  const [showModal, setShowModal] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
+
+  const toggleModal = (event: any) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = rect.left + window.scrollX;
+    const y = rect.bottom + window.scrollY;
+    setModalPosition({ x, y });
+    setShowModal(!showModal);
+  };
+
+  const handleLogout = () => {
+    // Remove the token from local storage
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    // Redirect the user to the login page or any other desired location
+    window.location.href = "/";
+  };
 
   return (
     <>
       <div className={Styles.container}>
         <div className={Styles.wrapper}>
-          <p className={Styles.title}>Welcome {user?.firstname}</p>
+          <div className={Styles.headerWrapper}>
+            <p className={Styles.title}>Welcome {user?.firstname}</p>
+            <img
+              src={profilePic}
+              alt="Profile Picture"
+              className={Styles.profilePic}
+              onClick={toggleModal}
+            />
+            {showModal && (
+              <LogoutModal onLogout={handleLogout} position={modalPosition} />
+            )}
+          </div>
           <div className={Styles.boxContainer}>
             {/* Box 1 */}
             <div className={Styles.box}>
