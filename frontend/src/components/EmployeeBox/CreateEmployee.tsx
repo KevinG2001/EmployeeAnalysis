209013bc181251e232e8d55806a30238eff7e3b3
Kevin <kevinglennon01@gmail.com>
2024-04-22 23:28:02 +0100

    Employee text fields setup

--- frontend/src/components/EmployeeBox/CreateEmployee.tsx
+++ frontend/src/components/EmployeeBox/CreateEmployee.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Style from "../../styles/EmployeeBox/createEmployee.module.scss";
 function CreateEmployee() {
-  return <div>Create Employee</div>;
+  const [firstname, setFirstname] = useState("");
+  const [surname, setSurname] = useState("");
+  const [dob, setDob] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isManager, setIsManager] = useState("");
+
+  return (
+    <>
+      <div className={Style.container}>
+        <div className={Style.row}>
+          <input type="text" placeholder="Firstname" />
+          <input type="text" placeholder="Surname" />
+        </div>
+        <div className={Style.row}>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="DOB" />
+        </div>
+        <div className={Style.row}>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className={Style.row}>
+          <div>Manager?</div>
+          <input type="checkbox" />
+        </div>
+        <div className={Style.row}>
+          <button className={Style.submitBtn}>Submit</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateEmployee;
