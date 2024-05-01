ca6c4594963c22bcee472503d3e0246f2416e12d
Kevin <kevinglennon01@gmail.com>
2024-05-01 02:48:18 +0100

    Route setup for employee profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
+import EmployeeProfile from "./pages/EmployeeProfile";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/profile/:employeeId" element={<EmployeeProfile />} />
         </Routes>
       </div>
     </Router>
